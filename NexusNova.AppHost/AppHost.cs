74abfee0923cc1b2437005591adc1cd8d80bb28d
LuisiitoDev <luisasandovalcoder@gmail.com>
2026-07-12 11:44:39 -0600

    Register NexusNova projects and update references
    Added NexusNova_Api and NexusNova to the distributed app builder in AppHost.cs. Updated NexusNova.AppHost.csproj to reference NexusNova.Api and NexusNova.Mobile, specifying net10.0 for the mobile project.

--- NexusNova.AppHost/AppHost.cs
+++ NexusNova.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.NexusNova_Api>("nexusnova-api");
+
+builder.AddProject<Projects.NexusNova>("nexusnova");
+
 builder.Build().Run();
